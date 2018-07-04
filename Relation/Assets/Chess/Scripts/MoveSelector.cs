4cd13f430ef68d6067a00906dc561cccceee5944
Kevin <paledust@hotmail.com>
2018-07-03 20:09:44 -0400

    ChessMyHeart

---
+++ Relation/Assets/Chess/Scripts/MoveSelector.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSelector : MonoBehaviour {
+	public GameObject moveLocationPrefab;
+	public GameObject tileHightlightPrefab;
+	public GameObject attackLocationPrefab;
+
+	private GameObject tileHightlight;
+	private GameObject movingPiece;
+	private List<Vector2Int> moveLocations;
+	private List<GameObject> locationHightlights;
+	// Use this for initialization
+	void Start () {
+		this.enabled = false;
+		tileHightlight = Instantiate(tileHightlightPrefab, Geometry.PointFromGrid(new Vector2Int(0,0)), Quaternion.identity, gameObject.transform);
+		tileHightlight.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)){
+			Vector3 point = hit.point;
+			Vector2Int gridPoint = Geometry.GridFromPoint(point);
+
+			tileHightlight.SetActive(true);
+			tileHightlight.transform.position = Geometry.PointFromGrid(gridPoint);
+			if(Input.GetMouseButtonDown(0)){
+				if(!moveLocations.Contains(gridPoint)) return;
+
+				if(GameManager.instance.PieceAtGrid(gridPoint) == null){
+					GameManager.instance.Move(movingPiece, gridPoint);
+				}
+				else{
+					GameManager.instance.CapturePieceAt(gridPoint);
+					GameManager.instance.Move(movingPiece, gridPoint);
+				}
+				ExitState();
+			}
+		}
+		else{
+			tileHightlight.SetActive(false);
+		}
+	}
+
+	public void EnterState(GameObject piece){
+		movingPiece = piece;
+		this.enabled = true;
+
+		moveLocations = GameManager.instance.MovesForPiece(movingPiece);
+		locationHightlights = new List<GameObject>();
+
+		foreach(Vector2Int loc in moveLocations){
+			GameObject hightlight;
+			if(GameManager.instance.PieceAtGrid(loc)){
+				hightlight = Instantiate(attackLocationPrefab, Geometry.PointFromGrid(loc),
+					Quaternion.identity, gameObject.transform);
+			}
+			else{
+				hightlight = Instantiate(moveLocationPrefab, Geometry.PointFromGrid(loc),
+					Quaternion.identity, gameObject.transform);
+			}
+			locationHightlights.Add(hightlight);
+		}
+	}
+	public void ExitState(){
+		this.enabled = false;
+		tileHightlight.SetActive(false);
+		GameManager.instance.DeselectPiece(movingPiece);
+		movingPiece = null;
+		TileSelector selector = GetComponent<TileSelector>();
+		GameManager.instance.NextPlayer();
+		selector.EnterState();
+
+		foreach(GameObject hightlight in locationHightlights){
+			Destroy(hightlight);
+		}
+	}
+
+}
---
+++ Relation/Assets/Chess/Scripts/TileSelector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelector : MonoBehaviour {
+	public GameObject tileHighlightPrefeb;
+	private GameObject tileHighlight;
+
+	// Use this for initialization
+	void Start () {
+		Vector2Int gridPoint = Geometry.GridPoint(0,0);
+		Vector3 point = Geometry.PointFromGrid(gridPoint);
+		tileHighlight = Instantiate(tileHighlightPrefeb, point, Quaternion.identity, gameObject.transform);
+		tileHighlight.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+		
+		if(Physics.Raycast(ray, out hit)){
+			Vector3 point = hit.point;
+			Vector2Int gridPoint = Geometry.GridFromPoint(point);
+
+			tileHighlight.SetActive(true);
+			tileHighlight.transform.position = Geometry.PointFromGrid(gridPoint);
+
+			if(Input.GetMouseButtonDown(0)){
+				GameObject seletectedPiece = 
+					GameManager.instance.PieceAtGrid(gridPoint);
+				if(GameManager.instance.DoesPieceBelongToCurrentPlayer(seletectedPiece)){
+					GameManager.instance.SelectPiece(seletectedPiece);
+					ExitState(seletectedPiece);
+				}
+			}
+		}
+		else{
+			tileHighlight.SetActive(false);
+		}
+	}
+	public void EnterState(){
+		enabled = true;
+	}
+	public void ExitState(GameObject movingPieces){
+		this.enabled = false;
+		tileHighlight.SetActive(false);
+		MoveSelector move = GetComponent<MoveSelector>();
+		move.EnterState(movingPieces);
+	}
+}
