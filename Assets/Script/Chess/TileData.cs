f30d45ecf853cdb71f818afd0cabefb8cb332ff4
paledust <paledust@hotmail.com>
2024-04-30 00:16:18 +0800

    update enum collection

--- Assets/Script/Chess/TileData.cs
+++ Assets/Script/Chess/TileData.cs
@@ -7,7 +7,7 @@ public struct TileData
     public CONTEXT_ENVIRONMENT environment;
     public CONTEXT_MOMENT moment;
     public void RND_TileData(){
-        environment = (CONTEXT_ENVIRONMENT)Random.Range(0, 7);
-        moment = (CONTEXT_MOMENT)Random.Range(0, 6);
+        environment = (CONTEXT_ENVIRONMENT)Random.Range(0, 5);
+        moment = (CONTEXT_MOMENT)Random.Range(0, 5);
     }
 }
